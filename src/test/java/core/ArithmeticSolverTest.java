c3136a3c16868824ce4e4d3095cb78a141ba0cae
Mostapha Zine El Abidine <mzine027@uottawa.ca>
2018-09-16 12:16:35 -0400

    Editing the existing files

--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -1,37 +1,37 @@
- package core;
+package core;
 
- import junit.framework.testcase;
- 
-public class ArithmeticSolverTest extends TestCase {
-	
-	public void TestAdd(){
+import junit.framework.TestCase;
+
+public class ArithmeticSolverTest extends TestCase{
+
+	public void testAdd(){
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(2, solver.add(2,2));
+		assertEquals(4.0, solver.add(2,2));
 		
 	}
 
-	public void TestSubstract() {
+	public void testSubstract() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(4, solver.substract(4,2));
+		assertEquals(2.0, solver.substract(4,2));
 	}
 	
-	public void TestMultiply() {
+	public void testMultiply() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(8, solver.multiply(4,2));
+		assertEquals(8.0, solver.multiply(4,2));
 	}
 	
-	public void TestDivide() {
+	public void testDivide() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(3, solver.divide(9,3));
+		assertEquals(3.0, solver.divide(9,3));
 	}
 	
-	public void TestModulus() {
+	public void testModulus() {
 		ArithmeticSolver solver = new ArithmeticSolver();
 		
-		assertEquals(1, solver.moduls(5,2));
+		assertEquals(1.0, solver.modulus(5,2));
 	}
 }
